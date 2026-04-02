7bfc393d5bbbf052527d4ea864688a57c50d7981
trmkhnva <tuurmakhanovaa@mail.ru>
2026-04-03 00:13:28 +0500

    Time: 0 ms (100.00%) | Memory: 21.9 MB (98.23%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.py
@@ -0,0 +1,24 @@
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left:
+                return root.right
+            if not root.right:
+                return root.left
+            
+            # Находим минимальный узел в правом поддереве
+            min_node = root.right
+            while min_node.left:
+                min_node = min_node.left
+            
+            root.val = min_node.val
+            root.right = self.deleteNode(root.right, min_node.val)
+        
+        return root
